62bad27ea545348c22db36852866716c277da888
HS <ehehqp@gmail.com>
2026-05-08 12:00:00 +0900

    제목: Lv4-2-3 테스트 코드 연습 본문: 매니저서비스 로직에서 일정의 유저가 null일 때 비즈니스 검증에러를 반환하는 로직 추가 및 메세지 일치화

--- src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
+++ src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
@@ -68,7 +68,7 @@ class ManagerServiceTest {
             managerService.saveManager(authUser, todoId, managerSaveRequest)
         );
 
-        assertEquals("일정을 생성한 유저만 담당자를 지정할 수 있습니다.", exception.getMessage());
+        assertEquals("일정의 유저를 찾을 수 없습니다.", exception.getMessage());
     }
 
     @Test // 테스트코드 샘플
