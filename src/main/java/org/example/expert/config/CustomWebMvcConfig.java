6198e8bc1367bc6410f4c36dbf1eccd08fc2a51f
HS <ehehqp@gmail.com>
2026-05-09 10:45:07 +0900

    제목: Lv5 Mvc설정에 인터셉터 추가 본문:

--- src/main/java/org/example/expert/config/CustomWebMvcConfig.java
+++ src/main/java/org/example/expert/config/CustomWebMvcConfig.java
@@ -4,18 +4,25 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class CustomWebMvcConfig implements WebMvcConfigurer {
 	private final AuthUserArgumentResolver authUserArgumentResolver;
-
-	public CustomWebMvcConfig(AuthUserArgumentResolver authUserArgumentResolver) {
-		this.authUserArgumentResolver = authUserArgumentResolver;
-	}
+	private final AdminCheckInterceptor adminCheckInterceptor;
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(authUserArgumentResolver);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(adminCheckInterceptor)
+			.addPathPatterns("/admin/comments/**", "/admin/users/**");
 	}
 }
